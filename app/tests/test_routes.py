8fd9181de4dbe05ae2d436ab7305e858d7b3eec8
Jande Royalty <janderoyalty@gmail.com>
2022-05-05 17:47:50 -0500

    Project Complete

--- app/tests/test_routes.py
+++ app/tests/test_routes.py
@@ -38,3 +38,33 @@ def test_get_one_planet_from_fixture_successfully(client, two_saved_planets):
     "length_of_year": 88,
     "id": 1
     }
+
+def test_get_one_planet_with_no_data_returns_404(client):
+    #act
+    response = client.get("/planets/1")
+
+    response_body = response.get_json()
+    #assert
+    assert response.status_code == 404
+    assert response_body == {'message': 'planet 1 not found'}
+
+def test_get_planet_from_fixture_successfully(client, two_saved_planets):
+    #act
+    response = client.get("/planets")
+
+    response_body = response.get_json()
+    #assert
+    assert response.status_code == 200
+    assert response_body == [{"name": "Mercury",
+    "description": "made mostly of rocks",
+    "circumference": 9522,
+    "length_of_year": 88,
+    "id": 1
+    },
+    {
+	"name": "Venus",
+	"description": "most like Earth",
+	"circumference": 23617,
+	"length_of_year": 225,
+    'id': 2,
+    }]
