a5f903fb3ee237a580b2f2f233456b7dae7bdbb7
Lindsey Still <lindseymstill@gmail.com>
2022-05-06 04:47:13 +0900

    wrote test_get_one_planet_from_fixture

--- app/tests/conftest.py
+++ app/tests/conftest.py
@@ -1,4 +1,5 @@
 import pytest
+from app.models.planet import Planet
 from app import create_app
 from app import db
 from flask.signals import request_finished
@@ -24,3 +25,15 @@ def app():
 @pytest.fixture
 def client(app):
 	return app.test_client()
+
+@pytest.fixture
+def two_saved_planets(app):
+# Arrange
+    mercury = Planet(name="Mercury",
+    description="made mostly of rocks", circumference=9522, length_of_year = 88)
+    venus = Planet(name="Venus",
+    description="most like Earth", circumference=23617, length_of_year=225)
+
+    db.session.add_all([mercury, venus])
+    # Alternatively, we could do# db.session.add(mercury)# db.session.add(venus)
+    db.session.commit()
--- app/tests/test_routes.py
+++ app/tests/test_routes.py
@@ -24,3 +24,17 @@ def test_create_one_planet(client):
     # Assert
     assert response.status_code == 201
     assert response_body == "Planet Goofy has been successfully created"
+
+def test_get_one_planet_from_fixture_successfully(client, two_saved_planets):
+    #act
+    response = client.get("/planets/1")
+
+    response_body = response.get_json()
+    #assert
+    assert response.status_code == 200
+    assert response_body == {"name": "Mercury",
+    "description": "made mostly of rocks",
+    "circumference": 9522,
+    "length_of_year": 88,
+    "id": 1
+    }
