ba996be7208b1c3c51b3cc20e3ff99bbe9bf2959
Lindsey Still <lindseymstill@gmail.com>
2022-05-11 02:59:08 +0900

    removed comments

--- app/routes.py
+++ app/routes.py
@@ -44,21 +44,6 @@ def get_all_planets():
 		planets_response.append(planet.to_json())
 			
 	return jsonify(planets_response)
-    
-
-# VALIDATE ID
-# def validate_planet(id):
-#     try:
-#         id = int(id)
-#     except:
-#         return abort(make_response({"message": f"planet {id} is invalid"}, 400))
-
-#     planet = Planet.query.get(id)
-
-#     if not planet:
-#         abort(make_response({"message":f"planet {id} not found"}, 404))
-    
-#     return planet
 
 # GET ONE PLANET
 @planets_bp.route("/<id>", methods = ["GET"])
